7fb1646a9617f4dbff3b724eb3e0718eb7340185
nminchow <noel@logrocket.com>
2020-07-23 14:23:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+👋
+
+This is a [node](https://nodejs.org/en/) project. Node is nice because there is a module for everything.
+
+Old projects would use npm for module management, but this project uses "[yarn](https://yarnpkg.com/)"
+
+Once you have node and yarn installed, you should be good to go. 
+
+## Running
+
+`yarn run`
+
+## Debugging
+
+You will need a debug environment set up, but once you do, `yarn debug` will start the file and attach to it for you.
---
+++ index.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const data = fs.readFileSync('urls.txt', 'UTF-8');
+
+// split the contents by new line
+const urls = data.split(/\r?\n/).filter(x => x);
+
+urls.map(url => {
+  axios.get(url)
+  .then(response => {
+    // You could also write to a file here, if that would be desirable.
+    // Note that you are in a `map` loop, so you will need to write to the file
+    // and ensure you get all of the responses before you close it.
+    console.log(url, response.status);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
